ffa353221f4dc736bda5123234b23adf2ba0b4e6
testdev1 <testdev1@sageit.me>
2021-12-09 10:09:14 -0600

    age.go

---
+++ age.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int
+	fmt.Println("my age is", age)
+	age = 25
+	fmt.Println("my age is", age)
+	age = 26
+	fmt.Println("my age is", age)
+}
